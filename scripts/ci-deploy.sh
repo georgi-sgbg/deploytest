549c3a14635ee5ccb86ad643d4bee87528279c44
Bill Hodghead <bhcrosslake@users.noreply.github.com>
2017-11-15 15:15:44 -0800

    Revert to CircleCI v1.0 build to simplify conversion

--- scripts/ci-deploy.sh
+++ scripts/ci-deploy.sh
@@ -2,9 +2,7 @@
 
 publishNpm() {
   # Push NPM package if not yet published
-   if [ "$(npm info $(npm ls --depth=-1 2>/dev/null | head -1 | cut -f 1 -d " ") 2>/dev/null)" = "undefined" ]; then
-    npm publish --access public
-  fi
+   npm publish --access public 2> /dev/null
 }
 
 pushDocker() {
